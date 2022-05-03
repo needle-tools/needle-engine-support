ea45f780efbe96fd239307dcf4fa5c0b52a9f6a8
Marcel Wiessler <marcel@gaisterhand.de>
2022-05-03 10:21:57 +0200

    add auth project

---
+++ starter/Authenticate/Assets/OpenCredentials.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+public static class OpenCredentials
+{
+#if UNITY_EDITOR
+	[InitializeOnLoadMethod]
+	private static async void Init()
+	{
+		await Task.Delay(2000);
+		SettingsService.OpenProjectSettings("Package Manager");
+
+	}
+
+#endif
+}
