121f4027d7612775a0b4ce45aa1c001839d3088a
Marcel Wiessler <marcel@gaisterhand.de>
2022-05-18 14:38:47 +0200

    start npmdef

---
+++ starter/Unity_2020_3/Assets/ExampleModules/MyFirstModule.codegen/Rotate.cs
@@ -0,0 +1,12 @@
+// auto generated code - do not edit
+namespace Needle.Typescript.GeneratedComponents
+{
+	public class Rotate : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.Vector3 @axes;
+		public bool @randomAxes = true;
+		public float @speed = 1f;
+		public void start(){}
+		public void update(){}
+	}
+}
---
+++ starter/Unity_2020_3/Assets/ExampleModules/MyFirstModule~/codegen/register_types.js
@@ -0,0 +1,7 @@
+﻿import { TypeStore } from "needle.tiny.engine/engine/engine_typestore"
+
+// Import types
+import { Rotate } from "../Rotate.ts";
+
+// Register types
+TypeStore.add("Rotate", Rotate);
