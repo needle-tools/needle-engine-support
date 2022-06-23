71b76260fee518355d475011ac51ce0cc75a7648
Marcel Wiessler <marcel@gaisterhand.de>
2022-06-23 18:00:41 +0200

    multi scene lightmaps demo

---
+++ starter/Unity_2020_3/Assets/Scenes/Lightmaps/com.needle.lightmaps-example.codegen/LoadScenes.cs
@@ -0,0 +1,12 @@
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class LoadScenes : UnityEngine.MonoBehaviour
+	{
+		public UnityEditor.SceneAsset[] @scenes;
+		public void start(){}
+	}
+}
---
+++ starter/Unity_2020_3/Assets/Scenes/Lightmaps/com.needle.lightmaps-example~/codegen/register_types.js
@@ -0,0 +1,7 @@
+﻿import { TypeStore } from "needle.tiny.engine/engine/engine_typestore"
+
+// Import types
+import { LoadScenes } from "../LoadScenes.ts";
+
+// Register types
+TypeStore.add("LoadScenes", LoadScenes);
