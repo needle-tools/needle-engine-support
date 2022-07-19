4acb2e6fc8e21c5650d47baffeb7a7728261cc21
Marcel Wiessler <marcel@gaisterhand.de>
2022-07-19 09:21:56 +0200

    Fix auth 2022 compat

--- starter/Authenticate/Assets/AutoCredentialsCheck/OpenCredentials.cs
+++ starter/Authenticate/Assets/AutoCredentialsCheck/OpenCredentials.cs
@@ -12,6 +12,16 @@ public static class AutoCredentials
 		Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, "{0}", "Please open <a href=\"https://packages.needle.tools\">https://packages.needle.tools</a> in your browser and login using your github account. Then copy the credentials from the <i>Registry Info</i> window (top right button) to your clipboard and return to Unity.");
 	}
 
+	#if UNITY_2021_1_OR_NEWER
+	private static void OnHyperLinkClicked(EditorWindow arg1, HyperLinkClickedEventArgs arg2)
+	{
+		if (arg2.hyperLinkData.TryGetValue("href", out var path))
+		{
+			if(path.StartsWith("https://"))
+				Application.OpenURL(path);
+		}
+	}
+	#else
 	private static void OnHyperLinkClicked(object sender, EventArgs e)
 	{
 		if (e is EditorGUILayout.HyperLinkClickedEventArgs args)
@@ -23,6 +33,8 @@ public static class AutoCredentials
 			}
 		}
 	}
+	#endif
+
 
 #endif
 }
