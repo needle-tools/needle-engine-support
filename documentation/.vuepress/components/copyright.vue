a140a0b36edbf3974d1f7f62d6c46ce3be140da1
Marcel Wiessler <marcel@gaisterhand.de>
2022-10-24 12:28:43 +0200

    https and privacy policy link on home

--- documentation/.vuepress/components/copyright.vue
+++ documentation/.vuepress/components/copyright.vue
@@ -11,10 +11,17 @@ export default {}
     padding: 1em;
     font-size: .8em;
 }
+
+a {
+    color: rgba(0, 0, 0, 0.8);
+    font-weight: normal;
+    margin-left: 5px;
+}
 </style>
 
 <template>
     <div class="footer">
-        Copyright © 2022 Needle Tools GmbH — Made with 🌵 in Germany
+        Copyright © 2022 Needle Tools GmbH — Made with 🌵 in Germany — <a target="_blank"
+            href="https://needle.tools/contact#privacy-policy">Privacy Policy</a>
     </div>
 </template>
