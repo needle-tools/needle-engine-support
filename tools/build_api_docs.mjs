acbdad7a6fc20b903b4bddd1f5495311f3aefebf
Marcel Wiessler <marcel@gaisterhand.de>
2024-03-29 12:32:44 +0100

    docs

--- tools/build_api_docs.mjs
+++ tools/build_api_docs.mjs
@@ -2,21 +2,22 @@ import { execSync } from 'child_process';
 import TypeDoc from 'typedoc';
 import fs from 'fs';
 
-console.log("Generating API documentation is disabled")
-process.exit(0);
+// console.log("Generating API documentation is disabled")
+// process.exit(0);
 
 // run npm command
 // const cmd = "typedoc --plugin typedoc-plugin-markdown --out documentation/api node_modules/@needle-tools/engine/src/needle-engine.ts";
 // execSync(cmd);
 
 const currentDirectory = process.cwd();
-const needleEngineDir = "C:/git/needle-tiny-playground/modules/needle-tiny/js/package~"
-const entryPoints = [
-    needleEngineDir,
-];
-const tsConfigPath = needleEngineDir + "/tsconfig.json";
+const needleEngineDir = "/Users/marcelwiessler/git/needle-engine-dev/modules/needle-engine/js/package~"
+// const tsConfigPath = needleEngineDir + "/tsconfig.json";
 const outputDirectory = "documentation/api";
 const outputDirectoryFull = currentDirectory + "/" + outputDirectory;
+const packageJsonPath = needleEngineDir + "/package.json";
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const packageName = packageJson.name;
+const packageVersion = packageJson.version;
 
 // delete output directory if it exists
 if (fs.existsSync(outputDirectoryFull))
@@ -24,21 +25,16 @@ if (fs.existsSync(outputDirectoryFull))
 
 console.log(currentDirectory);
 // console.log(tsConfigPath);
-console.log(entryPoints);
 
 async function main() {
-    const app = new TypeDoc.Application();
+    const app = await TypeDoc.Application.bootstrapWithPlugins({
+        entryPoints: [needleEngineDir + "/src/needle-engine.ts"],
+        // entryPointStrategy: "Packages",
+        // tsConfigPath: tsConfigPath,
 
-    // If you want TypeDoc to load tsconfig.json / typedoc.json files
-    app.options.addReader(new TypeDoc.TSConfigReader());
-    // app.options.addReader(new TypeDoc.TypeDocReader());
-    app.bootstrap({
-        entryPoints: entryPoints,
-        entryPointStrategy: "Packages",
         readme: "none",
-        tsconfig: tsConfigPath,
         disableSources: true,
-        // excludeExternals: true,
+        excludeExternals: true,
         skipErrorChecking: true,
         includeVersion: true,
         excludeInternal: true,
@@ -46,16 +42,16 @@ async function main() {
         excludePrivate: true,
         // exclude: "**/dist/**|**/_Ignore*.ts",
         // externalPattern: "**/node_modules/**",
-        excludeExternals: true,
         githubPages: false,
     });
-
-    const project = app.convert();
-
+    // If you want TypeDoc to load tsconfig.json / typedoc.json files
+    app.options.addReader(new TypeDoc.TSConfigReader());
+    const project = await app.convert();
     if (project) {
         // Project may not have converted correctly
-        const outputDir = "documentation/api";
+        const outputDir = "documentation/api/" + packageName + "/" + packageVersion;
         await app.generateDocs(project, outputDir);
+        // await app.generateJson(project, outputDir + "/documentation.json");
     }
 }
 
