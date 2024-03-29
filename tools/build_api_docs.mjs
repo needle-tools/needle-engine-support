816f580977138ca920aed7d2b0c846541beca31b
Marcel Wiessler <marcel@gaisterhand.de>
2024-03-29 15:26:18 +0100

    download and generate api from latest engine

--- tools/build_api_docs.mjs
+++ tools/build_api_docs.mjs
@@ -1,6 +1,7 @@
 import { execSync } from 'child_process';
 import TypeDoc from 'typedoc';
-import fs from 'fs';
+import fs, { WriteStream, mkdir, mkdirSync, rmSync } from 'fs';
+import fetch from 'node-fetch';
 
 // console.log("Generating API documentation is disabled")
 // process.exit(0);
@@ -9,26 +10,50 @@ import fs from 'fs';
 // const cmd = "typedoc --plugin typedoc-plugin-markdown --out documentation/api node_modules/@needle-tools/engine/src/needle-engine.ts";
 // execSync(cmd);
 
-const currentDirectory = process.cwd();
-const needleEngineDir = "/Users/marcelwiessler/git/needle-engine-dev/modules/needle-engine/js/package~"
-// const tsConfigPath = needleEngineDir + "/tsconfig.json";
-const outputDirectory = "documentation/api";
-const outputDirectoryFull = currentDirectory + "/" + outputDirectory;
-const packageJsonPath = needleEngineDir + "/package.json";
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-const packageName = packageJson.name;
-const packageVersion = packageJson.version;
-
-// delete output directory if it exists
-if (fs.existsSync(outputDirectoryFull))
-    fs.rmSync(outputDirectoryFull, { recursive: true });
-
-console.log(currentDirectory);
-// console.log(tsConfigPath);
-
 async function main() {
+
+    // download and unzip latest needle-engine package
+    const npmEndpoint = "https://registry.npmjs.org/@needle-tools/engine";
+    const content = await fetch(npmEndpoint).then(res => res.json());
+    const latestVersion = content["dist-tags"].latest;
+    const version = content.versions[latestVersion];
+    const tarball = version.dist.tarball;
+    const targetDirectory = process.cwd() + "/.temp/" + version.name + "-" + version.version;
+    if (!fs.existsSync(targetDirectory)) {
+        fs.mkdirSync(targetDirectory, { recursive: true });
+    }
+    else {
+        fs.rmSync(targetDirectory, { recursive: true });
+        fs.mkdirSync(targetDirectory, { recursive: true });
+    }
+    const tarballPath = targetDirectory + "/package.tgz";
+    const tarballStream = new WriteStream(tarballPath);
+    const response = await fetch(tarball);
+    response.body.pipe(tarballStream);
+    await new Promise((resolve, reject) => {
+        tarballStream.on('finish', resolve);
+        tarballStream.on('error', reject);
+    });
+    tarballStream.close();
+    const tarballDir = targetDirectory;
+    execSync("tar -xzf " + tarballPath + " -C " + tarballDir);
+    rmSync(tarballPath);
+
+    const packageDir = tarballDir + "/package";
+    const packageJsonPath = packageDir + "/package.json";
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+    const packageName = packageJson.name;
+    const packageVersion = packageJson.version;
+
+    const outputDirectory = "api/" + packageName + "/" + packageVersion;
+    const outputDirectoryFull = process.cwd() + "/" + outputDirectory;
+
+    // delete output directory if it exists
+    if (fs.existsSync(outputDirectoryFull))
+        fs.rmSync(outputDirectoryFull, { recursive: true });
+
     const app = await TypeDoc.Application.bootstrapWithPlugins({
-        entryPoints: [needleEngineDir + "/src/needle-engine.ts"],
+        entryPoints: [packageDir + "/src/needle-engine.ts"],
         // entryPointStrategy: "Packages",
         // tsConfigPath: tsConfigPath,
 
@@ -43,14 +68,17 @@ async function main() {
         // exclude: "**/dist/**|**/_Ignore*.ts",
         // externalPattern: "**/node_modules/**",
         githubPages: false,
+
+        // plugin: "typedoc-neo-theme",
+        // theme: "./node_modules/typedoc-neo-theme/bin/default",
     });
+
     // If you want TypeDoc to load tsconfig.json / typedoc.json files
     app.options.addReader(new TypeDoc.TSConfigReader());
     const project = await app.convert();
     if (project) {
         // Project may not have converted correctly
-        const outputDir = "documentation/api/" + packageName + "/" + packageVersion;
-        await app.generateDocs(project, outputDir);
+        await app.generateDocs(project, outputDirectory);
         // await app.generateJson(project, outputDir + "/documentation.json");
     }
 }
