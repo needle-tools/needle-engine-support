06b40f7500c8d056787aad440b63522203624391
Marcel Wiessler <marcel@gaisterhand.de>
2024-03-30 10:54:43 +0100

    api: whole automated uploading

--- tools/build_api_docs.mjs
+++ tools/build_api_docs.mjs
@@ -2,6 +2,11 @@ import { execSync } from 'child_process';
 import TypeDoc from 'typedoc';
 import fs, { WriteStream, mkdir, mkdirSync, rmSync } from 'fs';
 import fetch from 'node-fetch';
+import FtpDeploy from 'ftp-deploy';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 // console.log("Generating API documentation is disabled")
 // process.exit(0);
@@ -11,16 +16,67 @@ import fetch from 'node-fetch';
 // execSync(cmd);
 
 async function main() {
+    console.log("Cleaning up previous API documentation");
+    const baseDownloadDirectory = process.cwd() + "/.temp";
+    if (fs.existsSync(baseDownloadDirectory))
+        fs.rmSync(baseDownloadDirectory, { recursive: true });
 
     // download and unzip latest needle-engine package
-    console.log("Downloading latest needle-engine package");
     const npmEndpoint = "https://registry.npmjs.org/@needle-tools/engine";
     const content = await fetch(npmEndpoint).then(res => res.json());
-    const latestVersion = content["dist-tags"].latest;
-    const version = content.versions[latestVersion];
-    console.log("Latest version: " + latestVersion);
+
+    const needleEngineApiBaseUrl = "https://engine.needle.tools/docs/api";
+
+    for (const version of Object.keys(content.versions)) {
+
+        if (!version.startsWith("3.35")) continue;
+
+        const versionInfo = content.versions[version];
+
+        const url = needleEngineApiBaseUrl + "/" + versionInfo.name + "/" + versionInfo.version + "/index.html";
+
+        const response = await fetch(url, { method: "HEAD" });
+        if (response.status === 200) {
+            console.log("API documentation already exists for " + versionInfo.name + " " + versionInfo.version);
+            continue;
+        }
+
+        const targetDirectory = baseDownloadDirectory + "/" + versionInfo.name + "/" + versionInfo.version;
+        const tarballDir = await downloadPackage(versionInfo, targetDirectory);
+        console.log("Generating API documentation")
+        const packageDir = tarballDir + "/package";
+        const packageJsonPath = packageDir + "/package.json";
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+        const packageName = packageJson.name;
+        const packageVersion = packageJson.version;
+        const outputDirectory = "api/" + packageName + "/" + packageVersion;
+        const outputDirectoryFull = process.cwd() + "/" + outputDirectory;
+        // delete output directory if it exists
+        if (fs.existsSync(outputDirectoryFull))
+            fs.rmSync(outputDirectoryFull, { recursive: true });
+
+        const output = await produceDocs(packageDir, outputDirectoryFull);
+        console.log("API documentation generated at " + output);
+
+        await upload(output, packageName + "/" + packageVersion);
+        console.log("API documentation uploaded to " + packageName + "/" + packageVersion);
+        await delay(2000);
+    }
+
+}
+
+async function delay(ms) {
+    return new Promise(res => setTimeout(res, ms));
+}
+
+/** Download the package tarball and extract it to the target directory 
+ * @param {{dist:{tarball:string}}} version The version object from the npm registry
+ * @param {string} targetDirectory The target directory to extract the package to
+ * @returns {Promise<string>} The directory of the extracted package
+ **/
+async function downloadPackage(version, targetDirectory) {
+    console.log("Downloading needle-engine package: " + version.version + " to " + targetDirectory);
     const tarball = version.dist.tarball;
-    const targetDirectory = process.cwd() + "/.temp/" + version.name + "-" + version.version;
     if (!fs.existsSync(targetDirectory)) {
         fs.mkdirSync(targetDirectory, { recursive: true });
     }
@@ -42,52 +98,73 @@ async function main() {
     console.log("Extracting tarball to " + tarballDir);
     execSync("tar -xzf " + tarballPath + " -C " + tarballDir);
     rmSync(tarballPath);
+    return tarballDir;
+}
 
-    console.log("Generating API documentation")
-    const packageDir = tarballDir + "/package";
-    const packageJsonPath = packageDir + "/package.json";
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-    const packageName = packageJson.name;
-    const packageVersion = packageJson.version;
-    const outputDirectory = "api/" + packageName + "/" + packageVersion;
-    const outputDirectoryFull = process.cwd() + "/" + outputDirectory;
-    // delete output directory if it exists
-    if (fs.existsSync(outputDirectoryFull))
-        fs.rmSync(outputDirectoryFull, { recursive: true });
+/**
+ * Generate the API documentation for the given package directory
+ * @param {string} packageDir The directory of the package
+ * @param {string} outputDirectory The output directory for the documentation
+ * @returns {Promise<string | null>} The output directory
+ */
+async function produceDocs(packageDir, outputDirectory) {
+
+    const readmePath = packageDir + "/README.md";
 
     const app = await TypeDoc.Application.bootstrapWithPlugins({
-        entryPoints: [packageDir + "/src/needle-engine.ts"],
+        entryPoints: [
+            packageDir + "/src/engine/api.ts",
+            packageDir + "/src/engine-components/api.ts",
+            packageDir + "/src/engine-components-experimental/api.ts",
+            packageDir + "/src/engine-schemes/api.ts",
+            packageDir + "/src/needle-engine.ts",
+        ],
+        // don't include references multiple times
+        excludeReferences: true,
         // entryPointStrategy: "Packages",
         // tsConfigPath: tsConfigPath,
         // commentStyle: "all",
 
         readme: "none",
-        disableSources: true,
         excludeExternals: true,
         skipErrorChecking: true,
-        includeVersion: true,
         excludeInternal: true,
-        excludeProtected: true,
+        excludeProtected: false,
         excludePrivate: true,
 
         excludeNotDocumented: true,
-        excludeNotDocumentedKinds: ["Property", "Interface", "TypeAlias", "Enum", "Variable", "Function", ],
+        excludeNotDocumentedKinds: ["Property", "Interface", "TypeAlias", "Enum", "Variable", "Function",],
         // exclude: "**/dist/**|**/_Ignore*.ts",
         // externalPattern: "**/node_modules/**",
         githubPages: false,
+        includeVersion: false,
+        // don't print the local path (defined in .temp/...)
+        disableSources: true,
 
-        name: packageName,
-        includeVersion: true,
-        readme: packageDir + "/README.md",
+        name: "Needle Engine",
+
+        readme: readmePath,
         categorizeByGroup: true,
 
+        "visibilityFilters": {
+            "protected": false,
+            "inherited": false,
+            "@deprecated": false,
+        },
 
         // plugin: "typedoc-neo-theme",
         // theme: "./node_modules/typedoc-neo-theme/bin/default",
+
+        plugin: ["typedoc-plugin-inline-sources", "typedoc-plugin-mdn-links", "typedoc-plugin-keywords", "typedoc-plugin-extras"],
+        keywords: ["typescript", "library", "threejs", "webgl", "engine", "browser", "webxr", "api"],
+        footerDate: true,
     });
+
+    // inline sources plugin
 
     // If you want TypeDoc to load tsconfig.json / typedoc.json files
     app.options.addReader(new TypeDoc.TSConfigReader());
+
     console.log("Converting project");
     const project = await app.convert();
     if (project) {
@@ -95,7 +172,53 @@ async function main() {
         console.log("Generating documentation");
         await app.generateDocs(project, outputDirectory);
         // await app.generateJson(project, outputDir + "/documentation.json");
+        return outputDirectory;
     }
+
+    return null;
 }
+
+async function upload(directory, remotepath) {
+    console.log("Uploading to " + remotepath);
+
+    const user = process.env.API_FTP_USER;
+    const password = process.env.API_FTP_PASSWORD;
+    const host = process.env.API_FTP_HOST;
+
+    const ftpDeploy = new FtpDeploy();
+    const config = {
+        user,
+        // Password optional, prompted if none given
+        password,
+        host,
+        port: 21,
+        localRoot: directory,
+        remoteRoot: remotepath,
+        // upload everything except dot files
+        include: ["*", "**/*"],
+        // delete ALL existing files at destination before uploading, if true
+        deleteRemote: false,
+        // Passive mode is forced (EPSV command is not sent)
+        forcePasv: true,
+        // use sftp or ftp
+        sftp: false,
+    };
+    ftpDeploy.on("uploading", function (data) {
+        console.log("Uploading " + data.transferredFileCount + "/" + data.totalFilesCount + ": " + data.filename);
+    });
+    return new Promise(res => {
+        ftpDeploy
+            .deploy(config)
+            .then((result) => {
+                console.log("Finished uploading:", result);
+                res(result);
+            })
+            .catch((err) => {
+                console.log(err);
+                res(null);
+            });
+    });
+}
+
 
 main().catch(console.error);
