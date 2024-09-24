c176b6ce9a47c3e41703b840a25a28c552d4deb3
Marcel Wiessler <marcel@gaisterhand.de>
2024-09-24 17:17:30 +0200

    Fix website title

--- documentation/.vuepress/config.ts
+++ documentation/.vuepress/config.ts
@@ -31,7 +31,7 @@ dotenv.config()
 const _url = "https://engine.needle.tools/docs"
 const _base = "/docs/";
 
-const _title = "Documentation";
+const _title = "Needle Engine Documentation";
 const _description = "Needle Engine is a web-based runtime for 3D apps. It runs on your machine for development, and can be deployed anywhere. It is flexible, extensible, and collaboration and XR come naturally. Needle Exporter for Unity bridges the Unity Editor and the web runtime. It helps you to export your assets, animations, lightmaps and so on to the web. It is built around the glTF standard for 3D assets.";
 const _previewImg = "preview.jpeg";
 
