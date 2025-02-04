e048e9dd359058b5ea52dd6c1f03574472665a0a
hybridherbst <felix.herbst@gmail.com>
2025-02-05 00:32:25 +0100

    logs for missing links; might want to change this to an exception

--- documentation/.vuepress/config.ts
+++ documentation/.vuepress/config.ts
@@ -137,6 +137,8 @@ export default defineUserConfig({
                 onInitialized: async (app) => {
                     for (const page of app.pages) {
                         if (!page.filePathRelative) {
+                            // This one is allowed – only one that is expected to come from a virtual page
+                            if (page.path === "/404.html") continue;
                             console.error("Broken page", page.path);
                         }
                         const nonBrokenPages = app.pages.filter(x => x.filePathRelative);
@@ -144,7 +146,9 @@ export default defineUserConfig({
                         let allLinks = app.pages.flatMap(x => x.links).map(x => x.relative);
                         allLinks = allLinks.map(x => x.endsWith("/") ? x + "index.md" : x);
                         const missingLinks = allLinks.filter(x => !nonBrokenLinks.includes(x));
-                        console.log("Missing", missingLinks); //, allLinks, nonBrokenLinks);
+                        if (missingLinks.length > 0) {
+                            console.error("Missing Links!", missingLinks); //, allLinks, nonBrokenLinks);
+                        }
                     }
                     /*
                     console.log("app", app.pages.map(x => {
