8001d7d847c946ec679408c0f5bac084c8f624e7
Marcel Wiessler <marwie@users.noreply.github.com>
2025-03-05 10:59:23 +0100

    Update component-object-reference.ts

--- documentation/.vuepress/public/code-samples/component-object-reference.ts
+++ documentation/.vuepress/public/code-samples/component-object-reference.ts
@@ -1,4 +1,4 @@
-import { Behaviour, serializable } from "@needle-tools/engine";
+import { Behaviour, serializable, Camera } from "@needle-tools/engine";
 import { Object3D } from "three"
 
 export class MyClass extends Behaviour {
@@ -10,4 +10,8 @@ export class MyClass extends Behaviour {
     // Note that the @serializable decorator contains the array content type! (Object3D and not Object3D[])
     @serializable(Object3D) 
     myObjectReferenceList: Object3D[] | null = null;
+
+    // for component or other objects use the object's type
+    @serializable(Camera)
+    myCameraComponent: Camera | null = null;
 } 
